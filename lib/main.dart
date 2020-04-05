59bac14479f60cd9337993fb1bf720190b1cfbee
Ganeesha Chiranthani <ganeesha.94.11@gmail.com>
2020-04-05 13:10:03 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'pages/welcome.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+   @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return MaterialApp(
+      title: 'Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: WelcomePage(),
+    );
+  }
+}
+
+
