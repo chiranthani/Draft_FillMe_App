ce9d8b7e3d7066d052a9604d755a954473cc48a8
Ganeesha Chiranthani <ganeesha.94.11@gmail.com>
2020-04-07 21:07:08 +0530

    add firebase dependcy

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,56 @@
+import 'package:fillme/splashScreen.dart';
 import 'package:flutter/material.dart';
-import 'pages/welcome.dart';
+import 'login.dart';
+import 'mapHome.dart';
+import 'package:provider/provider.dart';
+import 'package:fillme/auth_session/get_session.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-   @override
+
+  @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
     return MaterialApp(
       title: 'Flutter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: WelcomePage(),
+      home: SplashScreen(),
     );
   }
 }
 
 
+//void main() => runApp(MultiProvider(
+//  providers: [
+//    ChangeNotifierProvider(
+//      builder: (context) => AuthSession(),
+//    ),
+//  ],
+//  child: MyApp(),
+//));
+//
+//class MyApp extends StatelessWidget {
+//   @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      title: 'Flutter',
+//      theme: ThemeData(
+//        primarySwatch: Colors.blue,
+//      ),
+//      debugShowCheckedModeBanner: false,
+//      home: Consumer<AuthSession>(
+//        builder: (context, notifier, child) {
+//          return notifier.user != null ? MyMapApp() : SplashScreen();
+//        },
+//      ),
+//    );
+//  }
+//}
+
+/*
+
+
+ */
